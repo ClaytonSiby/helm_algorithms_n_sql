58d4099b5fd0c80173c61bbe77fe2e0b621d6903
Clayton Siby <claytonsiby@gmail.com>
2023-10-13 07:47:11 +0200

    Add: solution for the contains method

--- algorithms/contains.py
+++ algorithms/contains.py
@@ -1,3 +1,12 @@
 
+import collections
+Node = collections.namedtuple('Node', ['value', 'left', 'right'])
+
 def contains(node, value):
-    pass
+    if node is None:
+        return False
+    if node.value == value:
+      return True
+    if value < node.value:
+        return contains(node.left, value)
+    return contains(node.right, value)
--- tests/test_contains.py
+++ tests/test_contains.py
@@ -8,5 +8,10 @@ def test_contains():
     n3 = Node(value=3, left=None, right=None)
     n2 = Node(value=2, left=n1, right=n3)
 
+    assert contains(n2, 1) is True
+    assert contains(n2, 2) is True
     assert contains(n2, 3) is True
     assert contains(n2, 4) is False
+
+    n4 = Node(value=4, left=None, right=None)
+    assert contains(n4, 1) is False
